f3bbf40ff53cedafb72c82b053fe4e35bb9dd294
jernicroy <jernicroy2000@gmail.com>
2024-06-30 19:00:33 +0530

    Log-back config Alive api and Actuator

---
+++ src/main/java/com/ro/portfolio/component/AliveService.java
@@ -0,0 +1,47 @@
+package com.ro.portfolio.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Description : Component to prevent application from spin down in the server
+ *
+ */
+@Component
+@EnableScheduling
+public class AliveService {
+    private final WebClient webClient;
+    @Value("${spring.actuator.health.url}")
+    private String healthCheckUrl;
+
+    private static final Logger log = LoggerFactory.getLogger(AliveService.class);
+    public AliveService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    @Scheduled(fixedRate = 60000) // Every 60 seconds
+    public void keepAlive() {
+        log.info(" Health check URL: "+ healthCheckUrl);
+        Mono<String> result = webClient.get()
+                .uri(healthCheckUrl)
+                .retrieve()
+                .bodyToMono(String.class);
+
+        result.subscribe(
+                response -> {
+                    // Handle successful response
+                    log.info("Health check response: " + response);
+                },
+                error -> {
+                    // Handle error
+                    log.error("Error during health check: " + error.getMessage());
+                }
+        );
+    }
+}
--- src/main/java/com/ro/portfolio/controller/VisitorInfoController.java
+++ src/main/java/com/ro/portfolio/controller/VisitorInfoController.java
@@ -2,6 +2,8 @@ package com.ro.portfolio.controller;
 
 import com.ro.portfolio.entity.VisitorsInfo;
 import com.ro.portfolio.service.VisitorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +17,12 @@ public class VisitorInfoController {
     @Autowired
     VisitorService visitorService;
 
+    public final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/fetch")
     public ResponseEntity<VisitorsInfo> fetchAndSaveVisitorInfo (){
         VisitorsInfo visitorsInfo = visitorService.fetchAndSaveProject();
+        log.info("Visitor fetch '/visitor/fetch' API succeed");
         return ResponseEntity.ok(visitorsInfo);
     }
 }
