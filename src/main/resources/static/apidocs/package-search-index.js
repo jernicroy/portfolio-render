b8f801747eef685affbbd67a6eaf374d3e167c25
jernicroy <jernicroy2000@gmail.com>
2024-08-15 20:24:54 +0530

    Added java docs

---
+++ src/main/resources/static/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ro.portfolio"},{"l":"com.ro.portfolio.component"},{"l":"com.ro.portfolio.config"},{"l":"com.ro.portfolio.controller"},{"l":"com.ro.portfolio.entity"},{"l":"com.ro.portfolio.model"},{"l":"com.ro.portfolio.repo"},{"l":"com.ro.portfolio.service"}];updateSearchResults();
